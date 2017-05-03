4ab3b6ce2b8510b2c2303f4c8ff8d38b99900594
Bernadette <bernadette@englemaninvest.com>
2017-05-03 17:09:06 -0400

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const React = require('react'),
+    ReactDOM = require('react-dom'),
+    Main = require('./components/Main');
+require('./css/stylesheet.css');
+
+
+ReactDOM.render(
+    <Main />, document.getElementById('mainContent')
+);
